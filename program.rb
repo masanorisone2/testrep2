04e98b48c13788eaee995130533e0ccd0c4e1696
unknown <113414A007Q6W@NES15G-A03853.nsl.ad.nec.co.jp>
2016-02-12 11:43:48 +0900

    new

---
+++ program.rb
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+require 'pry'
+require './CsvPlugin'
+
+c = CsvPlugin.new("zzzz")
+
+binding.pry
+
+binding.pry
+
+# 行を追加
+   # マスターで作業
+   # ブランチで作業
+   # ブランチを変更・マージ後変更
+binding.pry
+binding.pry
+# 別の行を追加
+
+c.run()
+c.showMethod()
+# 新たに修正
+# 新たに修正
+# 新たに修正
+# 新たに修正
+
+
+# 新たに修正
